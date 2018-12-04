5372825a2c31db9420faf55727932a9107919859
KOSTA <KOSTA@KOSTA-PC>
2018-12-04 10:39:36 +0900

    2

--- spring_websocket_1201/src/or/kosta/mvc/controller/MyChatController.java
+++ spring_websocket_1201/src/or/kosta/mvc/controller/MyChatController.java
@@ -1,5 +1,7 @@
 package or.kosta.mvc.controller;
 
+import javax.xml.ws.RequestWrapper;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +11,9 @@ public class MyChatController {
 	    @RequestMapping(value={"/chat","/"})
 	    public String viewChattingPage() {
 	        return "index2";
+	    }
+	    @RequestMapping("/test")
+	    public String message() {
+	    	return "test";
 	    }
 }
